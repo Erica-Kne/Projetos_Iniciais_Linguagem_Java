0eef2c85582951d2d75f1a97f071547db6054d6f
erica <erica-knevitz@outlook.com>
2025-06-20 22:10:16 -0300

    minhas mudanças iniciais no novo projeto de estoque

---
+++ Estoque_Produtos/src/entidades/Estoque.java
@@ -0,0 +1,40 @@
+package entidades;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Estoque {
+	
+	Scanner sc = new Scanner(System.in);
+	private List<Produto> estoque = new ArrayList<>();
+	
+	public void cadastrarProduto() {
+		
+		System.out.print("Nome do produto: ");
+		String nome = sc.nextLine().trim();
+		System.out.print("Quantidade: ");
+		int quantidade = sc.nextInt();
+		System.out.print("Preco: R$ ");
+		double preco = sc.nextDouble();
+		if(!nome.isEmpty() && quantidade >= 0 && preco > 0.0) {
+			
+			Produto produto = new Produto (nome, quantidade, preco);
+			estoque.add(produto);
+			System.out.println("Produto cadastrado.");
+			
+		} else {
+			System.out.println("Nao foi possivel cadastrar o produto:");
+			if(nome.isEmpty()) {
+				System.out.println("Nome invalido.");	
+			};
+			if( quantidade < 0) {
+				System.out.println("Quantidade invalida.");
+			};
+			if( preco <= 0.0) {
+				System.out.println("Preco invalido.");
+			}
+		}
+		sc.close();
+	};
+
+}
---
+++ Estoque_Produtos/src/entidades/Produto.java
@@ -0,0 +1,61 @@
+package entidades;
+
+public class Produto {
+	
+	private String nome;
+	private int quantidade = 0;
+	private double preco = 0.0;
+	private double valorTotal = 0.0;
+	
+	
+	
+	public Produto(String nome, int quantidade, double preco) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+		
+	};
+	
+	public Produto() {};
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public double getPreco() {
+		return preco;
+	};
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	};
+	
+	public String toString() {
+		return String.format("NOME: %s%nPRECO: %.2f%nQUANTIDADE: %d%n", nome, preco, quantidade);
+	};
+	
+	public void adicionarAoEstoque(int quantidade) {
+		this.quantidade += quantidade;
+	}
+	
+	public void removerDoEstoque(int quantidade) {
+		this.quantidade -= quantidade;
+	}
+	
+	public double getValorTotal() {
+		return quantidade * preco;
+	};
+	
+}
